7330cb54e2bdb8179a2c77a9d00038ce75526eb0
Kay Choi <kchoi@mail.sfsu.edu>
2018-05-06 20:29:24 -0700

    Added placeholder unit tests for Restaurant and Review Controllers.

---
+++ RestaurantReviewWebsite.Tests/Controllers/RestaurantControllerTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RestaurantReviewWebsite.Tests.Controllers
+{
+    /// <summary>
+    /// Summary description for RestaurantControllerTest
+    /// </summary>
+    [TestClass]
+    public class RestaurantControllerTest
+    {
+        [TestMethod]
+        public void Create()
+        {
+            // Arrange
+            //HomeController controller = new HomeController();
+
+            // Act
+            //ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            //Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+            // Arrange
+            //HomeController controller = new HomeController();
+
+            // Act
+            //ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            //Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void Details()
+        {
+            // Arrange
+            //HomeController controller = new HomeController();
+
+            // Act
+            //ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            //Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Search()
+        {
+            // Arrange
+            //HomeController controller = new HomeController();
+
+            // Act
+            //ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            //Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Update()
+        {
+            // Arrange
+            //HomeController controller = new HomeController();
+
+            // Act
+            //ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            //Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+        }
+    }
+}
---
+++ RestaurantReviewWebsite.Tests/Controllers/ReviewControllerTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RestaurantReviewWebsite.Tests.Controllers
+{
+    [TestClass]
+    public class ReviewControllerTest
+    {
+        [TestMethod]
+        public void Create()
+        {
+            // Arrange
+            //HomeController controller = new HomeController();
+
+            // Act
+            //ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            //Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+            // Arrange
+            //HomeController controller = new HomeController();
+
+            // Act
+            //ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            //Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void Details()
+        {
+            // Arrange
+            //HomeController controller = new HomeController();
+
+            // Act
+            //ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            //Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void List()
+        {
+            // Arrange
+            //HomeController controller = new HomeController();
+
+            // Act
+            //ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            //Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Update()
+        {
+            // Arrange
+            //HomeController controller = new HomeController();
+
+            // Act
+            //ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            //Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+        }
+    }
+}
