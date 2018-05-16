95a4d1d161150303ee451376ccad959b0add874d
Kay Choi <kay.n.choi@gmail.com>
2018-05-15 23:00:26 -0400

    Added WCF service.

---
+++ WCFServiceProvider/IRestaurantService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFServiceProvider
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IRestaurantService
+    {
+        [OperationContract]
+        Restaurant GetRestaurantByID(int id);
+
+        [OperationContract]
+        IEnumerable<Restaurant> GetRestaurants();
+        
+        [OperationContract]
+        IEnumerable<Review> GetReviewsByRestaurantID(int id);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class Restaurant
+    {
+        [DataMember]
+        public int ID { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Address { get; set; }
+        [DataMember]
+        public string City { get; set; }
+        [DataMember]
+        public string State { get; set; }
+        [DataMember]
+        public string ZIP { get; set; }
+        [DataMember]
+        public string PhoneNum { get; set; }
+        [DataMember]
+        public Nullable<double> AvgRating { get; set; }
+    }
+
+    [DataContract]
+    public class Review
+    {
+        [DataMember]
+        public int ID { get; set; }
+        [DataMember]
+        public int RestaurantID { get; set; }
+        [DataMember]
+        public Nullable<int> ReviewerID { get; set; }
+        [DataMember]
+        public int Rating { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+    }
+}
---
+++ WCFServiceProvider/RestaurantService.svc.cs
@@ -0,0 +1,95 @@
+﻿using DataProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFServiceProvider
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class RestaurantService : IRestaurantService
+    {
+        public Restaurant GetRestaurantByID(int id)
+        {
+            return Mapper.ConvertRestaurantFromDB(RestaurantCRUD.ReadRestaurants().Where(x => x.ID == id).FirstOrDefault());
+        }
+
+        public IEnumerable<Restaurant> GetRestaurants()
+        {
+            List<Restaurant> result = new List<Restaurant>();
+
+            foreach(var i in RestaurantCRUD.ReadRestaurants())
+            {
+                result.Add(Mapper.ConvertRestaurantFromDB(i));
+            }
+
+            return result;
+        }
+
+        public IEnumerable<Review> GetReviewsByRestaurantID(int id)
+        {
+            List<Review> result = new List<Review>();
+
+            foreach(var i in RestaurantCRUD.ReadReviews().Where(x => x.RestaurantID == id))
+            {
+                result.Add(Mapper.ConvertReviewFromDB(i));
+            }
+
+            return result;
+        }
+    }
+
+    static class Mapper
+    {
+        public static Restaurant ConvertRestaurantFromDB(DataProject.Restaurant rest)
+        {
+            Restaurant result;
+
+            if(rest != null)
+            {
+                result = new Restaurant
+                {
+                    Address = rest.Address,
+                    AvgRating = rest.AvgRating,
+                    City = rest.City,
+                    ID = rest.ID,
+                    Name = rest.Name,
+                    PhoneNum = rest.PhoneNum,
+                    State = rest.State,
+                    ZIP = rest.ZIP
+                };
+            } else
+            {
+                result = null;
+            }
+
+            return result;
+        }
+
+        public static Review ConvertReviewFromDB(DataProject.Review rev)
+        {
+            Review result;
+
+            if(rev != null)
+            {
+                result = new Review
+                {
+                    Description = rev.Description,
+                    ID = rev.ID,
+                    Rating = rev.Rating,
+                    RestaurantID = rev.RestaurantID,
+                    ReviewerID = rev.ReviewerID
+                };
+            } else
+            {
+                result = null;
+            }
+
+            return result;
+        }
+    }
+}
